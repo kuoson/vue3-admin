ef195c77d9861f7ec9082727d48bbcf37da9c2d4
kuosng <kuosng@163.com>
2024-02-19 14:09:46 +0800

    feat: 配置svg图片

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg>
+      <use :xlink:href="prefix + name" :fill="color"></use>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts" name="SvgIcon">
+defineProps({
+  prefix: {
+    type: String,
+    default: "#icon-",
+  },
+  name: String,
+  color: {
+    type: String,
+    defaut: "",
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 
+// 将登记的svg组件注册到全局
+import "virtual:svg-icons-register";
+
 const app = createApp(App);
 
 app.use(ElementPlus);
