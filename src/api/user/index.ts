e93319b1dc541be9c691c7c78f4cccaf1d9f4867
kuosng <kuosng@163.com>
2024-02-20 14:39:20 +0800

    feat: API接口统一管理

---
+++ src/api/user/index.ts
@@ -0,0 +1,14 @@
+import request from "@/utils/request";
+import type {
+  loginRequestData,
+  loginResponseData,
+  userInfoResponseData,
+} from "./type";
+
+export function login(data: loginRequestData) {
+  return request.post<any, loginResponseData>("/admin/acl/index/login", data);
+}
+
+export function getUserInfo() {
+  return request.get<any, userInfoResponseData>("/admin/acl/index/info");
+}
---
+++ src/api/user/type.ts
@@ -0,0 +1,23 @@
+interface ResponseData {
+  code: number;
+  message: string;
+}
+
+export interface loginRequestData {
+  username: string;
+  password: string;
+}
+
+export interface loginResponseData extends ResponseData {
+  data: string;
+}
+
+export interface userInfoResponseData extends ResponseData {
+  data: {
+    routes: string[];
+    buttons: string[];
+    roles: string[];
+    name: string;
+    avatar: string;
+  };
+}
