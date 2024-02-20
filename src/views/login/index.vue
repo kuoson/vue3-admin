a9b33f08fd5070081d25c9f275f669dede3214d6
kuosng <kuosng@163.com>
2024-02-20 22:08:39 +0800

    feat: 登录路由静态搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.username" />
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="handleLogin">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup lang="ts" name="Login"></script>
+<script setup lang="ts" name="Login">
+import { reactive } from "vue";
+import { User, Lock } from "@element-plus/icons-vue";
+
+let loginForm = reactive({
+  username: "",
+  password: "",
+});
+
+const handleLogin = () => {};
+</script>
