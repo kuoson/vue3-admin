167355c6092021649f188f6b35e0ec008533fed0
kuosng <kuosng@163.com>
2024-02-21 14:04:04 +0800

    feat: token持久化

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,15 +1,17 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { login as loginApi } from "@/api/user";
+import { getToken, setToken } from "@/utils/auth";
 import type { loginRequestData, loginResponseData } from "@/api/user/type";
 
 export const useUserStore = defineStore("user", () => {
-  const token = ref("");
+  const token: token = ref(getToken);
 
   const login = async (data: loginRequestData) => {
     const res: loginResponseData = await loginApi(data);
     if (res.code === 200) {
       token.value = res.data.token;
+      setToken(setToken);
     } else {
       return Promise.reject(new Error(res.message));
     }
---
+++ src/utils/auth.ts
@@ -0,0 +1,11 @@
+import Cookies from "js-cookie";
+
+const TOKEN_KEY = "TOKEN";
+
+export function getToken(): string {
+  return Cookies.get(TOKEN_KEY);
+}
+
+export function setToken(val) {
+  Cookies.set(TOKEN_KEY, val);
+}
