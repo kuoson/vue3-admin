65868ae93ad8b9461084246c76f1db3ac8c47b7e
kuosng <kuosng@163.com>
2024-02-22 09:22:15 +0800

    fix: layout组件静态搭建

---
+++ src/layout/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="layout-wrapper">
+    <div class="layout-sidebar">sidebar</div>
+    <div class="layout-tabbar">tabbar</div>
+    <div class="layout-main">content</div>
+  </div>
+</template>
+
+<script setup lang="ts" name="Layout"></script>
+
+<style scoped lang="scss">
+.layout-wrapper {
+  width: 100%;
+  height: 100vh;
+
+  .layout-sidebar {
+    width: $base-menu-width;
+    height: 100vh;
+  }
+
+  .layout-tabbar {
+    position: fixed;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+    top: 0px;
+    left: $base-menu-width;
+  }
+
+  .layout-main {
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>
