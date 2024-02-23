16486bd78a640d15f8aabedb0d82645d7236cf7d
kuosng <kuosng@163.com>
2024-02-23 21:41:45 +0800

    feat: 注册全局icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,15 @@
 import type { App, Component } from "vue";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import SvgIcon from "./SvgIcon/index.vue";
 
 const components: { [name: string]: Component } = { SvgIcon };
 
 export default {
   install(app: App) {
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component);
+    }
+
     Object.keys(components).forEach((key: string) => {
       app.component(key, components[key]);
     });
