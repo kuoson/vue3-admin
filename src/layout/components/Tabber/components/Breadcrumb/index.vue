c19818e851b5601c56a207dc8b167c37a3cb83ff
kuosng <kuosng@163.com>
2024-03-06 23:12:41 +0800

    feat: 面包屑动态展示 && 跳转

--- src/layout/components/Tabber/components/Breadcrumb/index.vue
+++ src/layout/components/Tabber/components/Breadcrumb/index.vue
@@ -1,21 +1,26 @@
 <template>
   <el-breadcrumb separator-icon="ArrowRight">
-    <el-breadcrumb-item>
-      <el-icon>
+    <!-- v-if 优先级比 v-for 高，会展示不出来；所以用 v-show -->
+    <el-breadcrumb-item
+      v-for="(item, index) in $route.matched"
+      :key="index"
+      v-show="item.meta.title"
+      :to="item.path"
+    >
+      <el-icon style="margin-right: 5px">
         <component :is="'HomeFilled'"></component>
       </el-icon>
-      <span>xxx</span>
-    </el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <el-icon>
-        <component :is="'HomeFilled'"></component>
-      </el-icon>
-      <span>yyy</span>
+      <span>{{ item.meta.title }}</span>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const $route = useRoute();
+console.log($route, 111);
+</script>
 
 <style scoped lang="scss">
 // data-v-xxx 没有传递给 el-breadcrumb__inner，需使用 :deep 包起来
