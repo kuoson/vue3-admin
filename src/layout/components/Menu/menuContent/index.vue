0b62ca45bf9e36c669dca000ec0cb1b49277bf2b
kuosng <kuosng@163.com>
2024-03-06 22:42:09 +0800

    fix: el-menu-item 菜单的 icon 丢失

--- src/layout/components/Menu/menuContent/index.vue
+++ src/layout/components/Menu/menuContent/index.vue
@@ -11,10 +11,10 @@
           :index="item.path"
           @click="goRoute"
         >
+          <el-icon>
+            <component :is="item?.meta?.icon"></component>
+          </el-icon>
           <template #title>
-            <el-icon>
-              <component :is="item?.meta?.icon"></component>
-            </el-icon>
             <span>
               {{ item?.meta?.title }}
             </span>
@@ -26,10 +26,10 @@
           :index="item?.children[0]?.path"
           @click="goRoute"
         >
+          <el-icon>
+            <component :is="item?.children[0]?.meta?.icon"></component>
+          </el-icon>
           <template #title>
-            <el-icon>
-              <component :is="item?.children[0]?.meta?.icon"></component>
-            </el-icon>
             <span>
               {{ item?.children[0]?.meta?.title }}
             </span>
