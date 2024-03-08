b0306fed85d90adeef1f84db7a54b5105138cf7d
kuosng <kuosng@163.com>
2024-03-08 20:19:57 +0800

    refactor: 刷新内容功能代码补充

--- src/layout/components/Main/index.vue
+++ src/layout/components/Main/index.vue
@@ -1,12 +1,28 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition name="fade">
-      <component :is="Component" :key="route.path" />
+      <component :is="Component" :key="route.path" v-if="renderFlag" />
     </transition>
   </router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch, nextTick } from "vue";
+import { useSettingStore } from "@/store/modules/setting";
+
+const settingStore = useSettingStore();
+const renderFlag = ref(true);
+
+watch(
+  () => settingStore.refresh,
+  () => {
+    renderFlag.value = false;
+    nextTick(() => {
+      renderFlag.value = true;
+    });
+  }
+);
+</script>
 
 <style scoped lang="scss">
 .fade-enter-from {
