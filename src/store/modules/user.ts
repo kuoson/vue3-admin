b55602a1b7b9f22354ddd071407139aa4b213793
kuosng <kuosng@163.com>
2024-03-08 21:24:39 +0800

    feat: 登陆成功后，接口携带 token &&  获取用户信息

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,21 +1,33 @@
 import { defineStore } from "pinia";
 import { ref, Ref } from "vue";
-import { login as loginApi } from "@/api/user";
+import { login as loginApi, getUserInfo } from "@/api/user";
 import { getToken, setToken } from "@/utils/auth";
 import type { loginRequestData, loginResponseData } from "@/api/user/type";
 
 export const useUserStore = defineStore("user", () => {
-  const token: Ref<() => string> = ref(getToken);
+  const token: Ref<string> = ref(getToken());
+  const username: Ref<string> = ref("");
+  const avatar: Ref<string> = ref("");
 
   const login = async (data: loginRequestData) => {
     const res: loginResponseData = await loginApi(data);
     if (res.code === 200) {
-      token.value = () => res.data.token;
-      setToken(setToken);
+      token.value = res.data.token;
+      setToken(token.value);
     } else {
       return Promise.reject(new Error(res.message));
     }
   };
 
-  return { token, login };
+  const userInfo = async () => {
+    const res = await getUserInfo();
+    if (res.code === 200) {
+      username.value = res.data.name;
+      avatar.value = res.data.avatar;
+    } else {
+      return Promise.reject(new Error(res.message));
+    }
+  };
+
+  return { token, login, userInfo };
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,4 +2,13 @@
   <div>home</div>
 </template>
 
-<script setup lang="ts" name="Home"></script>
+<script setup lang="ts" name="Home">
+import { onMounted } from "vue";
+import { useUserStore } from "@/store/modules/user";
+
+let userStore = useUserStore();
+
+onMounted(() => {
+  userStore.userInfo();
+});
+</script>
