62123fedc13b5740ec230e74a94b6a7ae73838f1
kuosng <kuosng@163.com>
2024-03-09 21:36:11 +0800

    feat: 新增商品管理操作接口

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,8 +1,16 @@
 import request from "@/utils/request";
-import type { TradeMarkResponseData } from "./type";
+import type { TradeMarkResponseData, TradeMark } from "./type";
 
 export function reqTradeMark(page: number, limit: number) {
   return request.get<any, TradeMarkResponseData>(
     `/admin/product/baseTrademark/${page}/${limit}`
   );
+}
+
+export function reqTradeMarkSave(data: TradeMark) {
+  return request.post<any, any>(`/admin/product/baseTrademark/save`, data);
+}
+
+export function reqTradeMarkUpdate(data: TradeMark) {
+  return request.put<any, any>(`/admin/product/baseTrademark/update`, data);
 }
