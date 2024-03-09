f69d6543f0a0b4f58fb5139bd0cdba8880b6eb7d
kuosng <kuosng@163.com>
2024-03-09 20:21:40 +0800

    fix: Transition 过渡需要提供一个根节点

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,53 +1,59 @@
 <template>
-  <el-card>
-    <el-button type="primary" icon="Plus" @click="handleAdd"
-      >添加平品牌</el-button
-    >
-    <el-table :data="tradeMarkArr" border style="margin-top: 10px">
-      <el-table-column prop="id" label="序号" width="80" algin="center" />
-      <el-table-column prop="tmName" label="品牌名称" />
-      <el-table-column label="品牌LOGO">
-        <template #="{ row, $index }">
-          <img :src="row.logoUrl" style="width: 100px; height: 100px" />
-        </template>
-      </el-table-column>
-      <el-table-column label="品牌操作">
-        <template #="{ row, $index }">
-          <el-button type="primary" icon="Edit" @click="handleEdit"></el-button>
-          <el-button type="danger" icon="Delete"></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-      style="margin-top: 10px"
-      v-model:current-page="currentPage"
-      v-model:page-size="pageSize"
-      :page-sizes="[3, 6, 9]"
-      background
-      layout="total, sizes, prev, pager, next, ->, jumper"
-      :total="total"
-      @size-change="handleSizeChange"
-      @current-change="getTradeMark"
-    />
-  </el-card>
+  <div>
+    <el-card>
+      <el-button type="primary" icon="Plus" @click="handleAdd"
+        >添加平品牌</el-button
+      >
+      <el-table :data="tradeMarkArr" border style="margin-top: 10px">
+        <el-table-column prop="id" label="序号" width="80" algin="center" />
+        <el-table-column prop="tmName" label="品牌名称" />
+        <el-table-column label="品牌LOGO">
+          <template #="{ row, $index }">
+            <img :src="row.logoUrl" style="width: 100px; height: 100px" />
+          </template>
+        </el-table-column>
+        <el-table-column label="品牌操作">
+          <template #="{ row, $index }">
+            <el-button
+              type="primary"
+              icon="Edit"
+              @click="handleEdit"
+            ></el-button>
+            <el-button type="danger" icon="Delete"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        style="margin-top: 10px"
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 6, 9]"
+        background
+        layout="total, sizes, prev, pager, next, ->, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="getTradeMark"
+      />
+    </el-card>
 
-  <el-dialog v-model="dialogVisible" title="添加品牌">
-    <el-form label-width="auto" style="width: 80%">
-      <el-form-item label="品牌名称">
-        <el-input placeholder="请输入品牌名称" />
-      </el-form-item>
-      <el-form-item label="品牌LOGO">
-        <el-upload class="avatar-uploader" :show-file-list="false">
-          <img v-if="''" :src="''" class="avatar" />
-          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-        </el-upload>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <el-button type="primary" @click="handleCancel">取消</el-button>
-      <el-button type="primary" @click="handleConfirm">确认</el-button>
-    </template>
-  </el-dialog>
+    <el-dialog v-model="dialogVisible" title="添加品牌">
+      <el-form label-width="auto" style="width: 80%">
+        <el-form-item label="品牌名称">
+          <el-input placeholder="请输入品牌名称" />
+        </el-form-item>
+        <el-form-item label="品牌LOGO">
+          <el-upload class="avatar-uploader" :show-file-list="false">
+            <img v-if="''" :src="''" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" @click="handleCancel">取消</el-button>
+        <el-button type="primary" @click="handleConfirm">确认</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
