de6967bf7983064778bf7558aaaafe98e2e01cde
kuosng <kuosng@163.com>
2024-03-09 11:38:43 +0800

    chore: 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,44 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import VitePluginVueSetupExtend from "vite-plugin-vue-setup-extend";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/dev-api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/dev-api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          changeOrigin: true,
+          rewrite: (path) =>
+            path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`, ""), ""),
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      symbolId: "icon-[dir]-[name]",
-    }),
-    VitePluginVueSetupExtend(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        symbolId: "icon-[dir]-[name]",
+      }),
+      VitePluginVueSetupExtend(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+  };
 });
