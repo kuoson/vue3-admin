de5db88e27286d5791a15d06b26163d45a390754
kuosng <kuosng@163.com>
2024-03-09 09:57:43 +0800

    refactor: 主页移除信息获取

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,4 @@
   <div>home</div>
 </template>
 
-<script setup lang="ts" name="Home">
-import { onMounted } from "vue";
-import { useUserStore } from "@/store/modules/user";
-
-let userStore = useUserStore();
-
-onMounted(() => {
-  userStore.userInfo();
-});
-</script>
+<script setup lang="ts" name="Home"></script>
