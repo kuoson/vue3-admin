6a6766f7a163a638797ef41e0e17c3a031549d27
kuosng <kuosng@163.com>
2024-03-09 13:23:16 +0800

    feat: element-plus 国际化配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import App from "./App.vue";
 
 import "@/styles/index.scss";
@@ -18,7 +19,9 @@ import pinia from "@/store";
 const app = createApp(App);
 
 app.use(gloablComponent);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.use(router);
 app.use(pinia);
 app.mount("#app");
