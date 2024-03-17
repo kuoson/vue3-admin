46ccdf747caa7ae5ea6dd97ba5ef0eeae41038d8
kuosng <kuosng@163.com>
2024-03-17 19:50:25 +0800

    feat: 属性管理静态页面

---
+++ src/views/product/attr/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Category />
+    <el-card style="margin-top: 10px">
+      <el-button type="primary" icon="Plus">添加平台属性</el-button>
+      <el-table style="margin-top: 10px" border>
+        <el-table-column label="序号" width="80" align="center" />
+        <el-table-column label="属性名称" width="150" />
+        <el-table-column label="属性值名称" />
+        <el-table-column label="操作" width="150" />
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Category from "@/components/Category/index.vue";
+</script>
