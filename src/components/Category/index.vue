3ee287a4e438911827ebd0763fb0d2b6c1a19651
kuosng <kuosng@163.com>
2024-03-17 19:50:40 +0800

    feat: 分类组件静态页面

---
+++ src/components/Category/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-card>
+    <el-form :inline="true">
+      <el-form-item label="一级分类">
+        <el-select :style="{ width: SELECT_WIDTH }"> </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select :style="{ width: SELECT_WIDTH }"></el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select :style="{ width: SELECT_WIDTH }"></el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+const SELECT_WIDTH = "200px";
+</script>
