178721afce333cc8ae2f927df051a60ada125270
kuosng <kuosng@163.com>
2024-03-24 13:53:35 +0800

    feat: spu数据展示

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -3,17 +3,53 @@
     <Category :isShowDataSence="isShowDataSence" />
     <el-card style="margin-top: 10px">
       <div>
-        <el-button type="primary" icon="Plus">添加SPU</el-button>
-        <el-table style="margin: 10px 0" border>
+        <el-button
+          type="primary"
+          icon="Plus"
+          :disabled="!categoryStore.category3Id"
+          >添加SPU</el-button
+        >
+        <el-table style="margin: 10px 0" border :data="spuArr">
           <el-table-column
             type="index"
             label="序号"
             width="80"
             align="center"
           />
-          <el-table-column label="SPU名称" />
-          <el-table-column label="SPU描述" />
-          <el-table-column label="操作" />
+          <el-table-column prop="spuName" label="SPU名称" />
+          <el-table-column
+            prop="description"
+            label="SPU描述"
+            show-overflow-tooltip
+          />
+          <el-table-column label="操作">
+            <template #="{ row, $index }">
+              <el-button
+                type="primary"
+                size="small"
+                icon="Plus"
+                title="添加SKU"
+              />
+              <el-button
+                type="warning"
+                size="small"
+                icon="Edit"
+                title="修改SPU"
+              />
+              <el-button
+                type="info"
+                size="small"
+                icon="View"
+                title="查看SKU列表"
+              />
+              <el-button
+                type="danger"
+                size="small"
+                icon="Delete"
+                title="删除SPU"
+              />
+            </template>
+          </el-table-column>
         </el-table>
         <el-pagination
           background
@@ -21,7 +57,9 @@
           v-model:page-size="pageSize"
           :page-sizes="[3, 6, 9]"
           layout="prev, pager, next, jumper, ->, sizes, total"
-          :total="9"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
         />
       </div>
     </el-card>
@@ -29,10 +67,48 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useCategoryStore } from "@/store/modules/category";
+import { reqSpu } from "@/api/product/spu";
+import type { Records } from "@/api/product/spu/type";
 import Category from "@/components/Category/index.vue";
 
+const categoryStore = useCategoryStore();
 const isShowDataSence = ref(true);
 const currentPage = ref(1);
 const pageSize = ref(3);
+const total = ref(0);
+const spuArr = ref<Records>([]);
+
+const getSpu = async () => {
+  const res = await reqSpu(
+    currentPage.value,
+    pageSize.value,
+    categoryStore.category3Id
+  );
+  if (res.code === 200) {
+    spuArr.value = res.data.records;
+    total.value = res.data.total;
+  }
+};
+
+const handleSizeChange = () => {
+  getSpu();
+};
+
+const handleCurrentChange = () => {
+  getSpu();
+};
+
+watch(
+  () => categoryStore.category3Id,
+  () => {
+    spuArr.value = [];
+    if (!categoryStore.category3Id) {
+      return;
+    }
+
+    getSpu();
+  }
+);
 </script>
