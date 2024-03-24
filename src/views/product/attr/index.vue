10ea661e427c00388df2aa6bc6479e772edc4b3b
kuosng <kuosng@163.com>
2024-03-24 12:32:31 +0800

    refactor: 属性管理序号为index

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -11,7 +11,12 @@
           >添加平台属性</el-button
         >
         <el-table style="margin-top: 10px" border :data="attrArr">
-          <el-table-column prop="id" label="序号" width="80" align="center" />
+          <el-table-column
+            type="index"
+            label="序号"
+            width="80"
+            align="center"
+          />
           <el-table-column prop="attrName" label="属性名称" width="150" />
           <el-table-column label="属性值名称">
             <template #="{ row, $index }">
